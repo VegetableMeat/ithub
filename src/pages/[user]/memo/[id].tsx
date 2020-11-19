6cb3562f13a928008a29f6f55a7561cc4869eb33
d-yamada <vegetable0518meat@gmail.com>
2020-11-19 03:03:03 +0000

    Add: 動的ルーティングの理解

---
+++ src/pages/[user]/memo/[id].tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const Id: NextPage = () => {
+  const router = useRouter();
+  const { user, id } = router.query;
+  return (
+    <div>
+      <p>
+        user:{user}さんの備忘録です。備忘録id:{id}
+      </p>
+    </div>
+  );
+};
+
+export default Id;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import Header from "../components/organisms/header";
 import styles from "../styles/Home.module.css";
+import Button from "../components/atoms/button/button";
 
-const Home: FC = () => {
+const Top: FC = () => {
+  const router = useRouter();
   return (
-    <div className={styles.container}>
+    // <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Button
+        text={"GO!!"}
+        btnFunc={() => router.push("/mochi/memo/mochi190")}
+      />
     </div>
   );
 };
 
-export default Home;
+export default Top;
