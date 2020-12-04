f5782d4730a1390107658d54d47906655d436e87
vegetablemeat <vegetable0518meat@gmail.com>
2020-12-04 00:38:39 +0000

    改善: TOPページのテストデータを移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,17 @@ import Navigation from "@/components/molecules/navigation";
 import Memo from "@/components/molecules/memo";
 import styles from "./style.module.css";
 
+import memo from "@/fixtures/top/memo.json";
+
 const Top: React.FC = () => {
-  // テストデータ
-  let test = [];
-
-  for (let i = 0; i < 10; i++) {
-    test.push({
-      image: {
-        src: "/icon/search.svg",
-        alt: "画像",
-        width: 50,
-        height: 50,
-      },
-      memo: {
-        title: "React + TypeScriptのチュートリアル",
-        icon: "R",
-        user_name: "React",
-        user_id: "react_type",
-        update_time: "1日前",
-        favorite: 10,
-      },
-    });
-  }
-
-  let memo = test.map((data) => <Memo image={data.image} memo={data.memo} />);
+  let memos = memo.map((data) => <Memo image={data.image} memo={data.memo} />);
 
   return (
     <Layout title="top">
       <div className={styles.topContainer}>
         <main className={styles.mainContainer}>
           <Navigation />
-          {memo}
+          {memos}
         </main>
       </div>
     </Layout>
