3fc5e03341c79cb0caf4c2e92f67cddad86c9444
d-yamada <vegetable0518meat@gmail.com>
2020-12-07 00:40:47 +0000

    改善: TOPページのメモ表示の改善

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,15 @@ import { API_URL } from "@/libs/api";
 
 const Top: React.FC = () => {
   const { data } = useSWR<Memo_entity[], Error>(`${API_URL}/top`);
-  // const memo = data.map((data) => <Memo memos={data} />);
+  console.log(data)
+  const memo = data.map((data) => <Memo memos={data} />);
 
   return (
     <Layout title="top">
       <div className={styles.topContainer}>
         <main className={styles.mainContainer}>
           <Navigation />
-          {memo}<Memo >
+          {memo}
         </main>
       </div>
     </Layout>
